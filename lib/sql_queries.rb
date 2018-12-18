0d88fc908d5d381a49d626292766f274ff224d6b
Tara Sabella <tarasabella@gmail.com>
2018-12-18 20:18:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT project.title, pledge.amount FROM pledges
-ORDER BY user_id ASC;"
+ORDER BY project.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
