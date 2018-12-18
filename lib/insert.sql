55a655faa4e6986327b0173396a6a78be2a78e65
Tara Sabella <tarasabella@gmail.com>
2018-12-18 20:04:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,7 +15,7 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
 (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
-INSERT INTO pledges (id, amount, user_id, project_id,) VALUES
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (1, 10.00, 1, 2),
 (2, 20.00, 1, 3),
 (3, 40.00, 1, 4),
