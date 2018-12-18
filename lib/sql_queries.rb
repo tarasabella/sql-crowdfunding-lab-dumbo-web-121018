880c55a91f06e97b03c519ccfac8bc3ab3fff657
Tara Sabella <tarasabella@gmail.com>
2018-12-18 20:02:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,8 +7,8 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, TEXT FROM projects
-ORDER BY amount ASC, INTEGER ASC;"
+"SELECT title, amount FROM projects
+ORDER BY  ASC, INTEGER ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
