172e66c4f264902bb9b6b03e074a496b380d4d86
Tara Sabella <tarasabella@gmail.com>
2018-12-18 19:30:51 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE project (id INTEGER PRIMARY KEY autoincrement, 
+title TEXT, 
+category TEXT, 
+funding_goal INTEGER, 
+start_date INTEGER,
+end_date TEXT);
+
+CREATE TABLE user (id INTEGER PRIMARY KEY autoincrement,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledge (id INTEGER PRIMARY KEY autoincrement;
+amount INTEGER, 
+user_id TEXT,
+project_id TEXT);
