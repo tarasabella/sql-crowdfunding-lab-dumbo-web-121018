4e7beb3bfea2bf551e730b8970a9ba8b74f9daba
Tara Sabella <tarasabella@gmail.com>
2018-12-18 21:26:13 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY autoincrement, 
+title TEXT, 
+category TEXT, 
+funding_goal INTEGER, 
+start_date TEXT,
+end_date TEXT);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY autoincrement,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY autoincrement,
+amount INTEGER, 
+user_id INTEGER,
+project_id INTEGER);
